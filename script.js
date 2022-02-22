b75703088c2ebdc29371f63f406fc9f89d0b5844
Thaysa <thay.fernnanda@gmail.com>
2022-02-22 17:43:23 -0300

    adiciona alteraçao de fundo

--- script.js
+++ script.js
@@ -4,9 +4,9 @@
 let botao = document.querySelector('#criar-tarefa');
 // console.log(botao);
 //disse o que o botão precisa fazer
-botao.addEventListener("click" , function(criarLi){
+botao.addEventListener("click", function (criarLi) {
     let input = document.querySelector('#texto-tarefa');
-console.log(input);
+    console.log(input);
     //criei um container onde sera adicionado as listas
     let container = document.getElementById('lista-tarefas');
     // console.log(container);
@@ -18,3 +18,17 @@ console.log(input);
     input.value = '';
 
 })
+// busquei a ol para criar um evento de click ao final
+    let buscarOl = document.querySelector('ol');
+//criei uma função para trocar o fundo
+function trocarFundo(event) {
+    //criei uma variavel para chamar a li
+    let lista = document.getElementsByTagName('li');
+    // fiz um for para percorrer a lista
+    for (let index = 0; index < lista.length; index += 1) {
+           lista[index].style.backgroundColor = null;
+        }
+    //implementando o evento
+        event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
+    buscarOl.addEventListener('click', trocarFundo);
